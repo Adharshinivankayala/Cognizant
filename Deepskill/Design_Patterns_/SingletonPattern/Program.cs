6cc4adc679a34223a2d386276eeafa4f2581d38c
Adharshinivankayala <204443666+Adharshinivankayala@users.noreply.github.com>
2026-06-20 21:19:41 +0530

    Add files via upload

---
+++ Deepskill/Design_Patterns_/SingletonPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿Logger logger1 = Logger.GetInstance();
+Logger logger2 = Logger.GetInstance();
+
+logger1.Log("Application Started");
+logger2.Log("Logging Information");
+
+if (logger1 == logger2)
+{
+    Console.WriteLine("Only one instance of Logger is created.");
+}
+else
+{
+    Console.WriteLine("Multiple instances are created.");
+}
